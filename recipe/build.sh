33a758d7fe8f31b28ffb3af43bbadc528012e402
Duncan Macleod <duncan.macleod@ligo.org>
2019-09-27 12:34:42 +0100

    fixed build failures on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,17 @@
 mkdir -p _build
 pushd _build
 
+# link librt to get clock_gettime on older glibc versions
+if [ "$(uname)" == "Linux" ]; then
+	export LDFLAGS="-lrt ${LDFLAGS}"
+fi
+
 # configure
 cmake ${SRC_DIR} \
 	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	-DCMAKE_BUILD_TYPE=RelWithDebInfo
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_INSTALL_LIBDIR="lib" \
+	-DDOXYGEN_EXECUTABLE=FALSE
 
 # build
 cmake --build . -- -j${CPU_COUNT}
